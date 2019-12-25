fae8e4390f3abc9e326c0bd9ab48d43794ce2aa4
beskhlebnyi <yaroslav@chimplie.com>
2019-12-25 16:03:29 +0200

    configure es for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,36 @@
 class Post < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings do
+    mappings do
+      indexes :author, type: :string
+      indexes :title, type: :string, analyzer: :english
+      indexes :body, type: :string, analyzer: :english
+      indexes :tags, type: :string, analyzer: :english
+      indexes :published, type: :boolean
+    end
+  end
+
+  def self.search_published(query)
+    self.search({
+      query: {
+        bool: {
+          must: [
+            {
+              multi_match: {
+               query: query,
+                fields: [:author, :title, :body, :tags]
+              }
+            },
+            {
+              match: {
+                published: true
+              }
+            }
+          ]
+        }
+      }
+    })
+  end
 end
